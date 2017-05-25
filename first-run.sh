8b0d3586e5d2e4177d0a8b4cb47b9b83c00eed2f
Chris Lozac'h <clozach@users.noreply.github.com>
2017-05-25 01:52:32 -0700

    first-run script no longer fails when invoked outside this repo.

--- first-run.sh
+++ first-run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-./mac-os-prefs.sh
-./shell-tools.sh
+###################
+# GENERAL PURPOSE #
+###################
+
+# PARTIAL CREDIT: https://github.com/kevinelliott/.dotfiles/blob/master/install.sh
+# HOW TO SHARE THIS ACROSS SCRIPTS?
+
+get_dot_to_dot_root() {
+  script="`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$0"`" #"`readlink -f "$0"`"
+  echo "`dirname "$script"`"
+}
+
+current_working_dir=$(get_dot_to_dot_root)
+
+$current_working_dir/mac-os-prefs.sh
+$current_working_dir/shell-tools.sh
