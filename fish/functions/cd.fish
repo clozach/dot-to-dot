97715475d19d04ae0ac6089d07a9001df9b288da
Chris Lozac'h <clozach@gmail.com>
2019-08-12 03:04:02 -0700

    Better `cd` — Filters out `.` `..` (via `-A` flag) and `.DS_Store` (via `grep`), all while still displaying dot files.

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -1,3 +1,7 @@
+# Defined in - @ line 2
 function cd --description 'List after every directory change'
-	builtin cd "$argv[1]"; echo; ls -a; echo;
+	builtin cd "$argv[1]"
+    echo
+    ls -A | grep -v ".DS_Store"
+    echo
 end
