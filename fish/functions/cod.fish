90ca08a6cc78accf70aab5efe5dfeaa5ee5bdaa9
Chris Lozac'h <clozach@gmail.com>
2019-10-08 16:56:46 -0700

    More `cod` functionality

--- fish/functions/cod.fish
+++ fish/functions/cod.fish
@@ -1,4 +1,16 @@
+# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.YelhGS/cod.fish @ line 2
 function cod --description 'Open `pwd` in VS Code & SourceTree'
 	code .
-stree .
+
+  stree .
+
+  switch (echo $argv)
+    case n
+      # n, meaning "no npm" does nothing more.
+
+    case '*'
+      # Launch the app
+      open http://localhost:5000
+      npm run dev
+  end
 end
