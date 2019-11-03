7caf9e5114ff17e538a89d735ece78336535f697
Chris Lozac'h <clozach@gmail.com>
2019-11-03 15:11:41 -0800

    Oops! Meant to use the builtin cd.

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.9tgeIE/cdf.fish @ line 1
 function cdf
-	cd ~/.config/fish/functions/
+    builtin cd ~/.config/fish/functions/
 end
