633b605c52d55a31d5738fcbe22494d5ba900a79
Chris Lozac'h <clozach@gmail.com>
2020-03-31 10:59:39 -0700

    New `save` function to wrap `funcsave`
    By opening my dot files repo in Sourcetree after every `funcsave`, I make it more likely that I'll keep the repo tidy without a bunch of uncommitted changes.

---
+++ fish/functions/save.fish
@@ -0,0 +1,6 @@
+# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.Clo23T/save.fish @ line 2
+function save --description 'Customizations to `funcsave`'
+	funcsave $argv
+
+  stree ~/git/dot-to-dot
+end
