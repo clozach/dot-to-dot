ce6a0566cd8e65bf103c0b3a8c1273dcaa202b08
Chris Lozac'h <clozach@gmail.com>
2020-04-05 19:34:58 -0700

    I moved my dot-to-dot dir locally, which broke this rather naively-written function

--- fish/functions/save.fish
+++ fish/functions/save.fish
@@ -1,6 +1,6 @@
-# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.Clo23T/save.fish @ line 2
+# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.Fc9B4h/save.fish @ line 2
 function save --description 'Customizations to `funcsave`'
 	funcsave $argv
 
-  stree ~/git/dot-to-dot
+  stree ~/Documents/2-Ongoing/dot-to-dot
 end
