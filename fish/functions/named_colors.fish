30db70b8c72526cc9e67c93c061e8e208eb285a9
Chris Lozac'h <clozach@gmail.com>
2020-04-05 18:36:24 -0700

    Print the colors listed by `set_color --print-colors` IN THE COLOR SPECIFIED!
    Why set_color doesn't offer this option is a mystery to me.
    
    Exercise for another time, perhaps: modify this to manipulate the output of `set_color --print-colors` in real time so that it's more robust. (There's not much likelihood that the colors will change much between now and whenever, but this would be a good way to practice shell manipulations of lists and strings.)

---
+++ fish/functions/named_colors.fish
@@ -0,0 +1,20 @@
+# Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.2Xzorz/named_colors.fish @ line 2
+function named_colors --description 'Print the named colors for use with `set_color` IN THEIR COLORS!'
+	echo (set_color black)black(set_color normal) – black
+	echo (set_color red)red(set_color normal) – red
+	echo (set_color green)green(set_color normal) – green
+	echo (set_color yellow)yellow(set_color normal) – yellow
+	echo (set_color blue)blue(set_color normal) – blue
+	echo (set_color magenta)magenta(set_color normal) – magenta
+	echo (set_color cyan)cyan(set_color normal) – cyan
+	echo (set_color white)white(set_color normal) – white
+	echo (set_color brblack)brblack(set_color normal) – brblack
+	echo (set_color brred)brred(set_color normal) – brred
+	echo (set_color brgreen)brgreen(set_color normal) – brgreen
+	echo (set_color bryellow)bryellow(set_color normal) – bryellow
+	echo (set_color brblue)brblue(set_color normal) – brblue
+	echo (set_color brmagenta)brmagenta(set_color normal) – brmagenta
+	echo (set_color brcyan)brcyan(set_color normal) – brcyan
+	echo (set_color brwhite)brwhite(set_color normal) – brwhite
+	echo (set_color normal)normal(set_color normal) – normal
+end
