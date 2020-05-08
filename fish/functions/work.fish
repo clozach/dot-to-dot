b8c33ae35467c276a3eb3be3ed2377dd31a7bd08
Chris Lozac'h <clozach@gmail.com>
2020-05-07 18:31:33 -0700

    Added `-i/--init` option to `work` function
    Cause I kept forgetting to make the file executable… 🙄

--- fish/functions/work.fish
+++ fish/functions/work.fish
@@ -1,22 +1,66 @@
 # Defined in /var/folders/7t/qn5djvd12cbf42936rdxpct40000gn/T//fish.qADUhN/work.fish @ line 2
 function work --description 'Quick launch of coding projects'
-	if set -q argv[1]
-		set loc $argv[1]
-	else
-		set loc .
-	end
 
-	code $loc
-	stree $loc
+    # Set local `options` with a default of h/help to support -h and --help
+    set -l options 'h/help' 'i/init'
 
-	set -l start_script $loc/work.eqg
+    # Parse incoming args into `options`
+    # E.g., `argparse -n myfunc $options -- $argv
+    #   -> If parsing fails, exit
+    argparse -n work $options -- $argv
+    or return
 
-	if [ -e $start_script ]
-		echo Launching $start_script
-		$start_script
-	end
+    # Print help if requested
+    #   -q == "query", i.e., check for this value
+    #   _flag_ is a special prefix. Below tests for either -h or --help
+    if set -q _flag_help
+        echo "The work does 3 simple things:
+				  1. Open the specified location (or current dir) in VS Code.
+				  2. Open the specified location (or current dir) in SourceTree.
+				  3. Runs the file `work.eqg` in that same directory, if there is one.
+					
+				Oh. And it can create a `work.eqg` file and make it executable, if you want.
 
-# The goal of this script is to strip away the inertia of getting back into an old project by making it easy to dive right back in.
-#
-# The work.eqg file is just a shell script that this `work` function can reference if there's a server to start, or any other prep to get back into the swing of things.
+				Example:
+					
+					cd ~/projects/newproj
+					work --init   # Opens the `work.eqg` file so you can add your startup instrutions.
+					              # Currently idiosyncratic: opens in VS Code.
+					work          # Opens the dev environment and rurns the script.
+				"
+
+        return 0
+    end
+
+    if set -q argv[1]
+        set loc $argv[1]
+    else
+        set loc .
+    end
+
+    set -l start_script $loc/work.eqg
+
+    if set -q _flag_init
+        if [ ! -e $start_script ]
+            touch $start_script
+        end
+
+        if [ ! -x $start_script ]
+            chmod +x $start_script
+        end
+
+        code $start_script
+    else
+        code $loc
+        stree $loc
+
+        if [ -e $start_script ]
+            echo Launching $start_script
+            $start_script
+        end
+    end
+
+    # The goal of this script is to strip away the inertia of getting back into an old project by making it easy to dive right back in.
+    #
+    # The work.eqg file is just a shell script that this `work` function can reference if there's a server to start, or any other prep to get back into the swing of things.
 end
